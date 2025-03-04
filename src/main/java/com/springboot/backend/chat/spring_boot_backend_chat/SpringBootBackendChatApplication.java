23c90139b9b989655f41e55ee458f0eca59deaac
cristiandaza07 <cristiandaza51@gmail.com>
2025-03-04 16:57:00 -0500

    Envio de mensajes desarrollado y funcionalidad de mostrar que usuario está escribiendo

---
+++ src/main/java/com/springboot/backend/chat/spring_boot_backend_chat/SpringBootBackendChatApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.backend.chat.spring_boot_backend_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/backend/chat/spring_boot_backend_chat/SpringBootBackendChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.backend.chat.spring_boot_backend_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
